832433275ce552d32de0a1ae698301345915acb0
Maksim Shevkoplias <maksim.shevkoplias@jetbrains.com>
2025-12-09 18:37:45 +0200

    softmax selection

--- ga_shapes/selection.py
+++ ga_shapes/selection.py
@@ -1,20 +1,53 @@
 import random
+from abc import ABC, abstractmethod
+
+import numpy as np
+from config import SELECTION_STRATEGY
 from config import TOURNAMENT_SIZE
 
-def tournament_selection(population, fitnesses):
-    """
-    Select one individual from population using tournament of k=TOURNAMENT_SIZE.
-    Returns the selected individual (Chromosome).
-    """
-    candidates = random.sample(list(zip(population, fitnesses)), TOURNAMENT_SIZE)
-    selected = max(candidates, key=lambda x: x[1])
-    return selected[0]
+class AbstractSelection(ABC):
+    @abstractmethod
+    def select(self,population, fitnesses):
+        pass
 
+class TournamentSelection(AbstractSelection):
+    def select(self, population, fitnesses):
+        """
+        Select one individual from population using tournament of k=TOURNAMENT_SIZE.
+        Returns the selected individual (Chromosome).
+        """
+        candidates = random.sample(list(zip(population, fitnesses)), TOURNAMENT_SIZE)
+        selected = max(candidates, key=lambda x: x[1])
+        return selected[0]
+
+class SoftmaxSelection(AbstractSelection):
+
+    def softmax(self,fitnesses):
+        exps = np.exp(fitnesses)
+        return exps / np.sum(exps)
+
+    def select(self,population, fitnesses):
+        """
+        Select one individual from population with probability proportional to softmax of fitness function.
+        Returns the selected individual (Chromosome).
+        """
+        probs = self.softmax(fitnesses)
+        cdf = np.cumsum(probs)
+        cdf[-1] = 1.0
+        r = np.random.rand()
+        idx = np.searchsorted(cdf, r, side="right")
+        return population[idx]
+
+SELECTIONS = {
+    "tournament": TournamentSelection,
+    "softmax": SoftmaxSelection
+}
 def select_parents(population, fitnesses):
     """
     Select two parents using tournament selection independently.
     """
+    selection = SELECTIONS[SELECTION_STRATEGY]()
     return (
-        tournament_selection(population, fitnesses),
-        tournament_selection(population, fitnesses),
+        selection.select(population, fitnesses),
+        selection.select(population, fitnesses),
     )
